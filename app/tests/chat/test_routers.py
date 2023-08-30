a779db03ac4519dbef9136afc4fc8466bdf74180
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2023-08-30 16:06:27 +0200

    Update chat tests

--- app/tests/chat/test_routers.py
+++ app/tests/chat/test_routers.py
@@ -1,8 +1,5 @@
-import asyncio
-import datetime
 import unittest
 
-import pytest
 from async_asgi_testclient import TestClient
 from fastapi import status
 from sqlalchemy import delete
@@ -11,50 +8,29 @@ from src.auth.jwt import create_access_token
 from src.auth.service import get_or_create_user
 from src.chat.schemas import RoomCreateWithUserId
 from src.chat.service import create_room_in_db
-from src.main import app
 from src.database import database, auth_user, room
-
-
-def mock_jwt_token():
-    return {"user_id": 123}
+from src.main import app
 
 
 class TestChat(unittest.IsolatedAsyncioTestCase):
-
-    # async def customSetUp(self) -> None:
 
     async def asyncSetUp(self) -> None:
         await database.connect()
         self.client = TestClient(app)
         self.user = await get_or_create_user({"email": "test_user@mail.com"})
         self.token = create_access_token(user=self.user)
-        room = await create_room_in_db(RoomCreateWithUserId(user_id=self.user.id, name="test_name"))
-        self.room_uuid = room.uuid
+        self.room_uuid = None
 
     async def asyncTearDown(self) -> None:
-        delete_query_1 = delete(room)
+        if self.room_uuid:
+            delete_query_1 = delete(room).where(room.c.uuid == self.room_uuid)
+            await database.execute(delete_query_1)
         delete_query = delete(auth_user).where(auth_user.c.email == "test_user@mail.com")
-        await database.execute(delete_query_1)
         await database.execute(delete_query)
+        await database.disconnect()
         self.user = None
 
-    # def setUpSync(self):
-    #     loop = asyncio.get_event_loop()
-    #     loop.run_until_complete(self.customSetUp())
-    #
-    # def tearDownSync(self):
-    #     loop = asyncio.get_event_loop()
-    #     loop.run_until_complete(self.customTearDown())
-
-    #
-    # async def customTearDown(self) -> None:
-    #     delete_query = delete(auth_user).where(auth_user.c.email) == "test_user@mail.com"
-    #     await database.fetch_one(delete_query)
-    #     self.user = None
-
     async def test_create_room(self) -> None:
-        # self.setUpSync()
-        # client = TestClient(app)
         resp = await self.client.post(
             "/chat/room",
             json={
@@ -64,13 +40,14 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
         )
         resp_json = resp.json()
         assert resp.status_code == status.HTTP_200_OK
-        # print(resp_json)
+        self.room_uuid = resp_json['room']['uuid']  # Store the room UUID for later deletion
         assert resp_json['room']['name'] == "MyRoom"
-        # assert resp_json['room']['created_at'] == datetime.datetime.now()
         assert "uuid" in resp_json['room']
         assert "user_id" in resp_json['room']
 
     async def test_update_room(self) -> None:
+        room = await create_room_in_db(RoomCreateWithUserId(user_id=self.user.id, name="test_name"))
+        self.room_uuid = room.uuid  # Store the room UUID for updating
         resp = await self.client.put(
             f"/chat/room/{self.room_uuid}",
             json={
@@ -80,7 +57,8 @@ class TestChat(unittest.IsolatedAsyncioTestCase):
         )
         resp_json = resp.json()
         assert resp.status_code == status.HTTP_200_OK
-        assert resp_json['room']['name'] == "PutName"
-    #
-    # async def test_get_rooms(self) -> None:
-    #     pass
+        assert resp_json['name'] == "PutName"
+
+
+if __name__ == "__main__":
+    unittest.main()
