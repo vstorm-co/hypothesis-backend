bde0f999323e5e4a036753f5efcf031303e682d6
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2023-09-01 12:12:03 +0200

    Format

--- app/src/chat/service.py
+++ app/src/chat/service.py
@@ -1,7 +1,7 @@
 import uuid
 
 from databases.interfaces import Record
-from sqlalchemy import insert, select, update, delete
+from sqlalchemy import delete, insert, select, update
 
 from src.chat.schemas import Message, RoomCreateWithUserId, RoomUpdateWithId
 from src.database import database, message, room
@@ -35,9 +35,7 @@ async def get_rooms_from_db(user_id) -> list[Record]:
 
 
 async def delete_room_from_db(room_id: str, user_id: int) -> Record | None:
-    delete_query = (
-        delete(room).where(room.c.uuid == room_id, room.c.user_id == user_id)
-    )
+    delete_query = delete(room).where(room.c.uuid == room_id, room.c.user_id == user_id)
     return await database.fetch_one(delete_query)
 
 
