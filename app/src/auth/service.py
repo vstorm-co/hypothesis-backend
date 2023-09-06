aa392c358b4b8ab325b687585ce1c3f42becbb37
Sobiech <p.sobieszczyk1@gmail.com>
2023-09-06 21:02:10 +0200

    fix auth service

--- app/src/auth/service.py
+++ app/src/auth/service.py
@@ -80,7 +80,7 @@ async def expire_refresh_token(refresh_token_uuid: UUID4) -> None:
     update_query = (
         refresh_tokens.update()
         .values(expires_at=datetime.utcnow() - timedelta(days=1))
-        .where(refresh_tokens.c.organization_uuid == refresh_token_uuid)
+        .where(refresh_tokens.c.uuid == refresh_token_uuid)
     )
 
     await database.execute(update_query)
