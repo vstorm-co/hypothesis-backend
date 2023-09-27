9d77040ed688860b1b92b56fe5d3b9c6e979a211
jakubJVStorm <108519375+jakubJVStorm@users.noreply.github.com>
2023-09-27 11:56:01 +0200

    Organizations login update (#34)
    * Add domain to organizations and add new endpoints to handle this
    
    * Format
    
    ---------
    
    Co-authored-by: JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>

--- app/src/database.py
+++ app/src/database.py
@@ -120,6 +120,7 @@ organization = Table(
     Column("name", String, unique=True, nullable=False),
     Column("picture", String, nullable=True),
     Column("created_at", DateTime, server_default=func.now(), nullable=False),
+    Column("domain", String, nullable=True),
 )
 
 organizations_users = Table(
