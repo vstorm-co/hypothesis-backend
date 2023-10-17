ac392504e38ae0a03863ab4054901ac976ea0d4c
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2023-10-17 13:26:08 +0200

    Change main and organization/utils

--- app/src/organizations/utils.py
+++ app/src/organizations/utils.py
@@ -6,7 +6,7 @@ from fastapi import UploadFile
 
 
 async def save_picture(file: UploadFile, dir_path: str):
-    if not Path(dir_path):
+    if not Path(dir_path).exists():
         os.mkdir(dir_path)
     async with aiofiles.open(dir_path + f"/{file.filename}", "wb") as out_file:
         content = await file.read()
