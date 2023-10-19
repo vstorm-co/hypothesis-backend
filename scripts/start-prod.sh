0b2b5a9345c64d45a2af8546421c8afd759f3eec
Sobiech <p.sobieszczyk1@gmail.com>
2023-10-19 20:30:29 +0200

    add gunicorn workers

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -11,6 +11,8 @@ export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 DEFAULT_GUNICORN_CONF=/src/gunicorn/gunicorn_conf.py
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
 export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+export WORKERS=${WORKERS:-1}
 
 # Start Gunicorn
-gunicorn --forwarded-allow-ips "*" -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+#gunicorn --forwarded-allow-ips "*" -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
+gunicorn --forwarded-allow-ips "*" -w "$WORKERS" -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
