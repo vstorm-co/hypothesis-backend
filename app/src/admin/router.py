3fac90bfab4249042fe9910d7ab2e03b1a6f5f18
JakubJedrzejak2000 <JakubJedrzejak2000@users.noreply.github.com>
2023-11-02 12:13:54 +0100

    Add Jira link

--- app/src/admin/router.py
+++ app/src/admin/router.py
@@ -10,6 +10,7 @@ router = APIRouter()
 
 
 # TODO Delete this
+# https://hypothesis-vstorm.atlassian.net/jira/software/projects/PA/boards/2?selectedIssue=PA-84
 @router.delete("")
 async def delete_database():
     await clean_entire_database()
--- app/src/admin/service.py
+++ app/src/admin/service.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 # TODO Delete this
+# https://hypothesis-vstorm.atlassian.net/jira/software/projects/PA/boards/2?selectedIssue=PA-84
 async def clean_entire_database() -> None:
     await database.fetch_all(delete(Organization))
     await database.fetch_all(delete(Room))
