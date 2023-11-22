d1af9b4b8eac4cc69acfd4687c0f418e48a81550
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2023-11-22 13:04:20 +0100

    123 gpt response is pre truncated and fixes (#68)
    * add room-updated info on message
    
    * PA-123 gpt response + info changed on message
    
    ---------
    
    Co-authored-by: piotr.sobieszczyk1 <piotr.sobieszczyk@ideamotive.com>

--- app/src/chat/chatting.py
+++ app/src/chat/chatting.py
@@ -5,7 +5,7 @@ from src.chat.schemas import MessageDB
 from src.chat.service import get_room_messages_from_db
 
 
-async def chat_with_chat(input_message: str, room_id: str):
+async def load_messages_history(room_id: str):
     db_messages = [
         MessageDB(**dict(message))
         for message in await get_room_messages_from_db(room_id)
@@ -17,13 +17,19 @@ async def chat_with_chat(input_message: str, room_id: str):
             "content": message.content,
         }
         for message in db_messages
-    ] + [{"role": "user", "content": input_message}]
+    ]
+
+    return messages
+
+
+async def chat_with_chat(input_message: str, room_id: str):
+    messages = await load_messages_history(room_id)
 
     try:
         async for chunk in await openai.ChatCompletion.acreate(
             api_key=chat_settings.CHATGPT_KEY,
             model="gpt-4-1106-preview",
-            messages=messages,
+            messages=messages + [{"role": "user", "content": input_message}],
             stream=True,
         ):
             if "content" in chunk["choices"][0]["delta"]:
