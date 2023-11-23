b7cfdf7c1e85aaabad2656fbc2c0f3fe2bd08882
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2023-11-23 11:07:33 +0100

    PA-120 Add new way of handling openai + add titles based on bot content (#70)

--- app/src/chat/chatting.py
+++ app/src/chat/chatting.py
@@ -1,38 +1,120 @@
-import openai
+from openai import AsyncClient, Client
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionFunctionMessageParam,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionToolMessageParam,
+    ChatCompletionUserMessageParam,
+)
 
 from src.chat.config import settings as chat_settings
-from src.chat.schemas import MessageDB
-from src.chat.service import get_room_messages_from_db
+from src.chat.schemas import MessageDB, RoomUpdateInputDetails
+from src.chat.service import get_room_messages_from_db, update_room_in_db
+from src.listener.manager import listener
 
 
-async def load_messages_history(room_id: str):
-    db_messages = [
-        MessageDB(**dict(message))
-        for message in await get_room_messages_from_db(room_id)
-    ]
+class HypoAI:
+    def __init__(
+        self, user_id: int, room_id: str, model_name: str = "gpt-4-1106-preview"
+    ):
+        self.user_id: int = user_id
+        self.room_id: str = room_id
+        self.model_name: str = model_name
 
-    messages = [
-        {
-            "role": "user" if message.created_by == "user" else "assistant",
-            "content": message.content,
-        }
-        for message in db_messages
-    ]
+        self.async_client: AsyncClient = AsyncClient(api_key=chat_settings.CHATGPT_KEY)
+        self.client: Client = Client(api_key=chat_settings.CHATGPT_KEY)
 
-    return messages
+        # update title
+        self.update_title: bool = False
 
+    async def load_messages_history(
+        self,
+    ) -> list[ChatCompletionUserMessageParam | ChatCompletionAssistantMessageParam]:
+        def type_cast(
+            message: MessageDB,
+        ) -> ChatCompletionUserMessageParam | ChatCompletionAssistantMessageParam:
+            if message.created_by == "user":
+                return ChatCompletionUserMessageParam(
+                    content=message.content, role="user"
+                )
+            else:
+                return ChatCompletionAssistantMessageParam(
+                    content=message.content, role="assistant"
+                )
 
-async def chat_with_chat(input_message: str, room_id: str):
-    messages = await load_messages_history(room_id)
+        db_messages = [
+            MessageDB(**dict(message))
+            for message in await get_room_messages_from_db(self.room_id)
+        ]
 
-    try:
-        async for chunk in await openai.ChatCompletion.acreate(
-            api_key=chat_settings.CHATGPT_KEY,
-            model="gpt-4-1106-preview",
-            messages=messages + [{"role": "user", "content": input_message}],
-            stream=True,
-        ):
-            if "content" in chunk["choices"][0]["delta"]:
-                yield chunk["choices"][0]["delta"]["content"]
-    except Exception as exc:
-        yield str(exc)
+        messages = [type_cast(message) for message in db_messages]
+
+        return messages
+
+    async def chat_with_chat(self, input_message: str):
+        messages: list[
+            ChatCompletionSystemMessageParam
+            | ChatCompletionUserMessageParam
+            | ChatCompletionAssistantMessageParam
+            | ChatCompletionToolMessageParam
+            | ChatCompletionFunctionMessageParam
+        ] = [
+            ChatCompletionSystemMessageParam(
+                content="You are a helpful assistant.", role="system"
+            ),
+        ]
+
+        messages_history = await self.load_messages_history()
+        if len(messages_history) == 1:
+            self.update_title = True
+
+        messages.extend(messages_history)
+
+        # update messages with user input
+        messages.append(
+            ChatCompletionUserMessageParam(content=input_message, role="user")
+        )
+
+        try:
+            # There is info that async_client.chat.completions.create
+            # has no __aiter__ method, even we use AsyncClient
+            # and stream=True
+            # this is probably a bug in openai library
+            # because we use client.chat.completions.create
+            # without any problems
+            # thus why we use type: ignore here
+            async for chunk in await self.async_client.chat.completions.create(  # type: ignore  # noqa: E501
+                model=self.model_name,
+                messages=messages,
+                stream=True,
+            ):
+                if chunk.choices and chunk.choices[0].delta.content:
+                    yield chunk.choices[0].delta.content
+        except Exception as exc:
+            yield str(exc)
+
+    async def update_chat_title(self, input_message: str):
+        if not self.update_title:
+            return
+
+        prompt = "Today, we’re going to create a prompt that will take a longish text, usually a prompt, and condense it to a very short “gist” of the text that the author will recognize when he or she sees it in a history that can only show about 25-30 characters of text. The gist should be a compact short sequence of words that make sense when said aloud, almost as a phrase or something. The gist may favor the first words in the prompt, or it may not, depending on how the prompt is structured. Are you ready for the text?"  # noqa: E501
+
+        completion = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": input_message},
+            ],
+        )
+        name = completion.choices[0].message.content
+
+        await update_room_in_db(
+            RoomUpdateInputDetails(
+                room_id=self.room_id,
+                user_id=self.user_id,
+                name=name,
+            )
+        )
+        await listener.receive_and_publish_message("room-changed")
+
+        self.update_title = False
