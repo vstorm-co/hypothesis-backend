a4b2b79ed397b7ed6d737e5ce1447b7f940e7926
Sobiechh <p.sobieszczyk1@gmail.com>
2024-02-08 13:19:40 +0100

    rollback add drive.readonly scope

--- app/src/auth/providers/google.py
+++ app/src/auth/providers/google.py
@@ -68,7 +68,7 @@ class GoogleAuthProviderFactory(AuthProviderFactory):
                 "openid",
                 "https://www.googleapis.com/auth/userinfo.profile",
                 "https://www.googleapis.com/auth/userinfo.email",
-                "https://www.googleapis.com/auth/drive.readonly",
+                # "https://www.googleapis.com/auth/drive.readonly",
             ],
             redirect_uri=REDIRECT_URI,
         )
