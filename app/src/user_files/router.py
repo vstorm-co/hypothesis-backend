8ed3ab99f6f155de5503b703030f611d725d802e
Sobiechh <p.sobieszczyk1@gmail.com>
2024-02-22 10:56:17 +0100

    Add update info on file upload

--- app/src/user_files/router.py
+++ app/src/user_files/router.py
@@ -5,6 +5,12 @@ from fastapi import APIRouter, Depends, UploadFile
 from src.auth.jwt import parse_jwt_user_data
 from src.auth.schemas import JWTData
 from src.chat.chatting import hypo_ai
+from src.listener.constants import (
+    optimizing_user_file_content_info,
+    user_file_updated_info,
+)
+from src.listener.manager import listener
+from src.listener.schemas import WSEventMessage
 from src.user_files.exceptions import (
     FailedToDownloadAndExtractFile,
     UserFileAlreadyExists,
@@ -60,12 +66,27 @@ async def create_user_file(
         data.title = hypo_ai.get_title_from_url(data.source_value)
         data.extension = data.source_value.split(".")[-1]
 
+    logger.info("Optimizing content...")
+    await listener.receive_and_publish_message(
+        WSEventMessage(
+            type=optimizing_user_file_content_info,
+            id=str(jwt_data.user_id),
+            source="update-user-file-content",
+        ).model_dump(mode="json")
+    )
     data.optimized_content = hypo_ai.optimize_content(data.content)
     user_file = await upsert_user_file_to_db(jwt_data.user_id, data)
 
     if not user_file:
         raise UserFileAlreadyExists()
 
+    await listener.receive_and_publish_message(
+        WSEventMessage(
+            type=user_file_updated_info,
+            id=str(jwt_data.user_id),
+            source="update-user-file-content",
+        ).model_dump(mode="json")
+    )
     return UserFileDB(**dict(user_file))
 
 
