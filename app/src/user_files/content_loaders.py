67a27691356c895d1d21dfcad80553f1be5fecde
Sobiechh <p.sobieszczyk1@gmail.com>
2024-03-07 00:19:03 +0100

    Add scraping page content

---
+++ app/src/user_files/content_loaders.py
@@ -0,0 +1,31 @@
+from io import BytesIO
+from logging import getLogger
+
+from docx import Document
+from langchain_community.document_transformers import BeautifulSoupTransformer
+
+from src.user_files.loaders import AsyncChromiumLoader
+
+logger = getLogger(__name__)
+
+
+def read_docx_from_bytes(content):
+    try:
+        doc = Document(BytesIO(content))
+        text = ""
+        for paragraph in doc.paragraphs:
+            text += paragraph.text + "\n"
+        return text
+    except Exception as e:
+        logger.error(f"Failed to read docx file: {e}")
+        return None
+
+
+async def get_content_from_url(url: str):
+    loader = AsyncChromiumLoader(urls=[url])
+    docs = await loader.load()
+
+    bs_transformer: BeautifulSoupTransformer = BeautifulSoupTransformer()
+    docs_transformed = bs_transformer.transform_documents(docs)
+
+    return docs_transformed[0].page_content
