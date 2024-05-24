d437b3aa82f013cd19a36af30c50446819c0cb0f
Sobiechh <p.sobieszczyk1@gmail.com>
2024-05-24 13:29:28 +0200

    - logging_production.ini

--- app/gunicorn/gunicorn_conf.py
+++ app/gunicorn/gunicorn_conf.py
@@ -47,4 +47,4 @@ accesslog = use_accesslog
 graceful_timeout = int(graceful_timeout_str)
 timeout = int(timeout_str)
 keepalive = int(keepalive_str)
-logconfig = "/home/papaya/backend/app/src/logging_production.ini"
+logconfig = "/home/papaya/backend/app/logging_production.ini"
