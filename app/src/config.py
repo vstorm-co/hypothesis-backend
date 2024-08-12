20acc522369ff5f04b0111e34331e797b6758613
Sobiechh <p.sobieszczyk1@gmail.com>
2024-08-12 16:36:27 +0200

    USer model Fernet key imports optimize - decode

--- app/src/config.py
+++ app/src/config.py
@@ -45,7 +45,7 @@ class Config(BaseSettings):
     LOGGING_CONFIG: str | None = "/home/papaya/backend/app/logging_production_file.ini"
 
     # Cryptography
-    FERNET_KEY: bytes = Fernet.generate_key()
+    FERNET_KEY: bytes = Fernet.generate_key().decode()
 
     # Youtube
     YOUTUBE_PROXY_URL: str | None = None
