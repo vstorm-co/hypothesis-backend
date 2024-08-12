d98470030b480e721aad7468520b357104f0fa87
Sobiechh <p.sobieszczyk1@gmail.com>
2024-08-12 16:44:40 +0200

    delete unused fernet import

--- app/src/config.py
+++ app/src/config.py
@@ -1,7 +1,6 @@
 from functools import lru_cache
 from typing import Any
 
-from cryptography.fernet import Fernet
 from pydantic import PostgresDsn, RedisDsn, model_validator
 from pydantic_settings import BaseSettings
 
